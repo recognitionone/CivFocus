e166c87be68a06f045a844a216d74753f2efa0e7
Basia <Basia@MBP-Basia.home>
2017-10-16 14:47:23 +0200

    new button

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello World Again</Text>
+        <Button          
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
@@ -15,8 +19,8 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 0,
+    backgroundColor: '#ff00cc',
     alignItems: 'center',
     justifyContent: 'center',
   },
