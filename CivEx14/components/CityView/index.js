a651012b52e09e3b7f4bfd836400ed8364d5dfe1
recognitionone <recognitionone@gmail.com>
2018-01-15 20:37:46 +0100

    new exercises

--- CivEx14/components/CityView/index.js
+++ CivEx14/components/CityView/index.js
@@ -6,55 +6,7 @@ export default class CityView extends Component{
     render(){
         const { navigate } = this.props.navigation;
         return( <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                  <Text> Hello World 1 </Text>
+                  <Text> Hello Again My World 1 </Text>
                 </View>)
       }
     }
-
-
-    
-    
-    
-    // class FocusRoomScreen extends React.Component {
-    //     constructor(props){
-    //       super(props);
-    //       this.state={
-    //         percent:0,
-    //         startAngle:0,
-    //         angleLength:1,
-    //         textValue:0,
-    //         time:moment(new Date()).format('MM:SS')
-    //       }
-    //       this.onBtnPress=this.onBtnPress.bind(this)
-    //       this.onUpdate=this.onUpdate.bind(this)
-    //     }
-    //     static navigationOptions = {
-    //       title: 'Welcome to Focus Room',
-    //     }
-      
-    //     onBtnPress(){
-    //       var x=this.state.percent
-    //       setInterval(function(){
-    //         if(x<100){
-    //           this.setState({percent:x+1})
-    //           x+=1
-    //         }
-    //       }.bind(this),1000)
-    //     }
-      
-    //     render() {
-    //       const { navigate } = this.props.navigation;
-    //       return( <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-    //                 {/* <PercentageCircle radius={100} borderWidth={20} 
-    //                 children={<Text>{this.state.percent}</Text>} percent={this.state.percent}
-    //                 textStyle	={{fontSize: 26, color: 'red'}}
-    //                 color={"#3498db"}></PercentageCircle>  */}
-    //                 <View style={styles.sleepTimeContainer}>
-    //                   <Text style={styles.textValue}>{this.state.textValue}</Text>
-    //                 </View>
-    //                 <Timer />
-    //                 <Text>{this.state.time}</Text>
-    //                 <Button title={'Start'} onPress={this.onBtnPress}/> 
-    //               </View>)
-    //     }
-    //   }    
--- CivEx14/components/Images/index.js
+++ CivEx14/components/Images/index.js
@@ -1,56 +1,25 @@
 import React, { Component } from 'react';
-import {View, Button, Text, TextInput, Slider} from 'react-native';
+import {View, Button, Text, TextInput, Slider, StyleSheet} from 'react-native';
 
-// import ActionButton from 'react-native-circular-action-menu';
-// import Icon from 'react-native-vector-icons/Ionicons';
-
-
-// export default class ImagesScreen extends Component {
-//   render(){
-//     const { navigate } = this.props.navigation;
-//     return (
-//         <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
-//           {/*Rest of App come ABOVE the action button component!*/}
-//           <ActionButton buttonColor="rgba(231,76,60,1)">
-//             <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
-//               <Icon name="android-create"  />
-//             </ActionButton.Item>
-//             <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-//               <Icon name="android-notifications-none"  />
-//             </ActionButton.Item>
-//             <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-//               <Icon name="android-done-all"  />
-//             </ActionButton.Item>
-//           </ActionButton>
-//         </View>
-//       );
-//     }
-//   }
-
-class Greetings extends Component {
+class Header extends Component {
   render() {
+    let myText = "lorem ipsum";
     return (
-      <Text style={{fontWeight: "bold"}}>Hello {this.props.name}!?</Text>
+      <View>
+        <Text>{myText}</Text>
+        <TextInput 
+          placeholder="What needs to be done"
+          blurOnSubmit={false}
+          returnKeyType="done"
+        />
+      </View>
     );
   }
 }
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {score: true};
-
-    setInterval(() => {
-      this.setState(prevState => {
-        return {
-          score: !prevState.score
-        }
-      })
-    }, 1000);
-  }
-
+class Footer extends Component {
   render() {
-    let display = this.state.score ? this.props.plext : 'o w dupę';
+    let display = 'dolor sum';
     return (
       <View>
         <Text>{display}!</Text>
@@ -59,142 +28,37 @@ class Blink extends Component {
   }
 }
 
-class Clock extends Component {
-  constructor(props) {
-    super(props);
-    this.state={leClock: new Date()}
-
-      setInterval(() => this.setState({
-        leClock: new Date()
-      }), 1000);
-  }
-
-  render() {
-    return(<Text>{this.state.leClock.toLocaleTimeString()}</Text>);
-  }
-}
-
-class Counter extends Component {
-  constructor(props){
-    super(props);
-    this.state = {count: 5};
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-
-    setInterval(() => this.setState({
-      count: this.state.count-1
-    }), 1000);
-  }
-
-  increment() {
-    const {count} = this.state;
-    this.setState({count: count + 1});
-  }
-
-  decrement() {
-    const {count} = this.state;
-    this.setState({count: count - 1});
-  }
-
-  render() {
-    const {count} = this.state;
-    return(<View  style={{flexDirection: 'row', alignContent:'space-between', 
-    margin: 30, justifyContent: 'space-between'}}>
-      <Text>{count}</Text>
-      <Button title="Decrement" onPress={this.decrement}>( - )</Button>
-      <Button title="Increment" onPress={this.increment}>( + )</Button>
-    </View>);
-  }
-}
-
-class MySlider extends Component {
-  constructor(props) {
-    super(props);
-    this.state={things:10};
-  }
-
-  // start() {
-  //   const {count} = this.state;
-  //   this.setState({count: count + 1});
-  // }
-
-  // stop() {
-  //   const {count} = this.state;
-  //   this.setState({count: count - 1});
-  // }
-
-
-  render() {
-   
-    return(
-    <View>
-      <Text>{this.state.things}</Text>
-      <Slider 
-        style={{ width: 300 }}
-        step={10}
-        minimumValue={0}
-        maximumValue={300}
-        value={this.state.things}
-        onValueChange={val => this.setState({ things: val })}  
-      />
-      {/* <Button title="Start" onPress={this.start}>START</Button>
-      <Button title="Stop" onPress={this.stop}>STOP</Button> */}
-    </View>
-    )
-  }
-}
-
 export default class ImagesScreen extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: 'oo', 
-      pftext: 360,
-    };
-
-    setInterval(() => this.setState({
-      pftext: this.state.pftext-1
-    }), 1000);
-
-  }
-
   render(){
     return(
-      <View style={{
-        flex:1, 
-        backgroundColor: '#f4f4f4', 
-        padding: 10, 
-        }}>
-
-        <TextInput 
-          style={{height: 40}}
-          placeholder="some placeholder"
-          placeholderTextColor="#000"
-          onChangeText={(text) => this.setState({text})}
-        />
-        
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && ' ').join('o')}
-          <Greetings name={this.state.text}/>
-        </Text>
-        
-        <View style={{borderBottomColor: '#a0a0a0', borderBottomWidth: 1}}/>
-        {/* <Blink plext='Po co ci kapusta'/> */}
-        <Text>This is my number that goes down: {this.state.pftext}</Text>
-        {/* <Clock /> */}
-        <Counter />
-        <View style={{borderBottomColor: '#a0a0a0', borderBottomWidth: 1}}/>
-        <View style={{flexDirection: 'row', 
-          alignContent:'space-between', 
-          margin: 30, 
-          justifyContent: 'space-between'}}>
-          <Text>Some text</Text>
-          <MySlider />
-        </View>
-        <View style={{borderBottomColor: '#a0a0a0', borderBottomWidth: 1}}/>
-
+      <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <Text>Hi</Text>
+      </View>
+      <Footer />
       </View>
     )
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0F0F0"
+  },
+  
+  content: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    paddingTop: 30
+  }, 
+  input:{
+    flex: 1,
+    height: 80,
+    backgroundColor: "#FFD800"
+  }
+
+  
+
+})
