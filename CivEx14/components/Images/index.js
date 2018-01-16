2a27611601a946958adc2802179fbcbd41beee0c
recognitionone <recognitionone@gmail.com>
2018-01-16 16:55:54 +0100

    todo list stuff

--- CivEx14/components/Images/index.js
+++ CivEx14/components/Images/index.js
@@ -2,16 +2,51 @@ import React, { Component } from 'react';
 import {View, Button, Text, TextInput, Slider, StyleSheet} from 'react-native';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      text: 'Useless Placeholder',
+      value: "",
+      items: []
+    };
+    this.handleAddItem = this.handleAddItem.bind(this);
+  }
+
+  handleAddItem() {
+    if(!this.state.value) return;
+    const newItems = [
+    ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value, 
+        complete: false
+      }
+    ]
+    this.setState ({
+      items: newItems,
+      value: ""
+    })
+  }
+
   render() {
-    let myText = "lorem ipsum";
+    let myText = "Hi hi: ";
     return (
-      <View>
+      <View style={styles.header}>
         <Text>{myText}</Text>
         <TextInput 
-          placeholder="What needs to be done"
+          placeholder="What needs to be done !!!!"
           blurOnSubmit={false}
           returnKeyType="done"
-        />
+
+          style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: '#FFD800', marginLeft: 1, paddingLeft: 8, minWidth: 150, justifyContent: "flex-start"}}
+          // onChangeText={(text) => this.setState({text})}
+
+          value={this.state.text}
+          onChange={ (value) => this.setState({ value })}
+
+          onAddItem={this.handleAddItem}
+
+          />
       </View>
     );
   }
@@ -19,7 +54,7 @@ class Header extends Component {
 
 class Footer extends Component {
   render() {
-    let display = 'dolor sum';
+    let display = 'dolor sum amet';
     return (
       <View>
         <Text>{display}!</Text>
@@ -44,8 +79,15 @@ export default class ImagesScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
     backgroundColor: "#F0F0F0"
+  },
+  header: {
+    // flex: 1,
+    paddingHorizontal: 2,
+    flexDirection: "row",
+    justifyContent: "flex-start", 
+    alignItems: "center"
   },
   
   content: {
@@ -55,7 +97,6 @@ const styles = StyleSheet.create({
   }, 
   input:{
     flex: 1,
-    height: 80,
     backgroundColor: "#FFD800"
   }
 
