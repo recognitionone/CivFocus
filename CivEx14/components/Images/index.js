372c9acda6313fb74bbb4b47ac65cb231f6b66ac
recognitionone <recognitionone@gmail.com>
2018-01-29 18:01:18 +0100

    somethin in images

--- CivEx14/components/Images/index.js
+++ CivEx14/components/Images/index.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
-import {View, Button, Text, TextInput, Slider, StyleSheet} from 'react-native';
+import {View, Button, Text, TextInput, Slider, StyleSheet, Touchable} from 'react-native';
+
+
+// class Inputs extends Component {
+//    state = {
+//       email: '',
+//       password: ''
+//    }
+//    handleEmail = (text) ⇒ {
+//       this.setState({ email: text })
+//    }
+//    handlePassword = (text) ⇒ {
+//       this.setState({ password: text })
+//    }
+//    login = (email, pass) ⇒ {
+//       alert('email: ' + email + ' password: ' + pass)
+//    }
+//    render(){
+//       return (
+//          <View style = {styles.container}>
+//             <TextInput style = {styles.input}
+//                underlineColorAndroid = "transparent"
+//                placeholder = "Email"
+//                placeholderTextColor = "#9a73ef"
+//                autoCapitalize = "none"
+//                onChangeText = {this.handleEmail}/>
+            
+//             <TextInput style = {styles.input}
+//                underlineColorAndroid = "transparent"
+//                placeholder = "Password"
+//                placeholderTextColor = "#9a73ef"
+//                autoCapitalize = "none"
+//                onChangeText = {this.handlePassword}/>
+               
+//             <TouchableOpacity
+//                style = {styles.submitButton}
+//                onPress = {
+//                   () ⇒ this.login(this.state.email, this.state.password)
+//                }>
+//                <Text style = {styles.submitButtonText}> Submit </Text>
+//             </TouchableOpacity>
+//          </View>
+//       )
+//    }
+// }
+
+
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      text: 'Useless Placeholder',
+      // text: 'Useless Placeholder',
       value: "",
       items: []
     };
@@ -14,6 +60,7 @@ class Header extends Component {
 
   handleAddItem() {
     if(!this.state.value) return;
+    
     const newItems = [
     ...this.state.items,
       {
@@ -22,29 +69,38 @@ class Header extends Component {
         complete: false
       }
     ]
+
     this.setState ({
       items: newItems,
       value: ""
     })
+    console.log("hello world");
   }
 
   render() {
-    let myText = "Hi hi: ";
+    let myText = "Hi";
     return (
       <View style={styles.header}>
-        <Text>{myText}</Text>
+        <Text>{myText}: </Text>
         <TextInput 
-          placeholder="What needs to be done !!!!"
+          placeholder="What needs to be done"
           blurOnSubmit={false}
-          returnKeyType="done"
+          returnKeyType={"done"}
 
           style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: '#FFD800', marginLeft: 1, paddingLeft: 8, minWidth: 150, justifyContent: "flex-start"}}
           // onChangeText={(text) => this.setState({text})}
 
-          value={this.state.text}
-          onChange={ (value) => this.setState({ value })}
 
-          onAddItem={this.handleAddItem}
+          // onChange={ (value) => this.setState({ value })}
+          onChangeText={ (value) => this.setState({value})}
+          
+          // onChangeText={this.props.onChange}
+
+          // onAddItem={this.handleAddItem}
+          onSubmitEditing={ () => this.handleAddItem}
+
+          value={this.state.value}
+          // onSubmitEditing={this.props.onAddItem}
 
           />
       </View>
@@ -95,10 +151,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#F5F5F5",
     paddingTop: 30
   }, 
-  input:{
-    flex: 1,
-    backgroundColor: "#FFD800"
-  }
+  // input:{
+  //   flex: 1,
+  //   backgroundColor: "#FFD800"
+  // }, 
+  input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 1
+   },
+   submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText:{
+      color: 'white'
+   }
 
   
 
