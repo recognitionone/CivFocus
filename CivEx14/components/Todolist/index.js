86d0d34bcef65bd960fdba4b78a4df826e576e54
recognitionone <recognitionone@gmail.com>
2018-02-02 19:39:20 +0100

    todolist working

--- CivEx14/components/Todolist/index.js
+++ CivEx14/components/Todolist/index.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Button, TextInput } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Button, TextInput, ListView, Keyboard} from 'react-native';
 import Header from '../Todolist/Header'
 import Footer from '../Todolist/Footer'
 
 
+
+class Row extends Component {
+
+  render() {
+        // <Text>{this.props.text}</Text>
+    return (
+      <View>
+        <Text>[{this.props.item.isComplete ? "Yyy" : "E."}]  {this.props.item.text}</Text>
+      </View>
+    )
+  }
+}
+
+// class TasksList extends Component { 
+//   constructor (props) { 
+//     super (props); 
+
+//     // const ds = new ListView.DataSource({ 
+//     //   rowHasChanged: (r1, r2) => r1 !== r2 
+//     // }); 
+
+//     this.state = { 
+//       // dataSource: ds.cloneWithRows([ 
+//       //   'Buy milk', 
+//       //   'Walk the dog', 
+//       //   'Do laundry', 
+//       //   'Write the first chapter of my book' 
+//       // ]) 
+//     }; 
+//   } 
+
+//   render () { 
+//     return ( 
+//       <ListView 
+//         style={styles.list}
+//         enableEmptySections
+//         dataSource={ this.state.dataSource } 
+//         onScroll={() => Keyboard.dismiss()}
+//         renderRow={ (key, ...value) => {
+//           return ( <Row key={key} {...value} /> )
+//         }} 
+//         renderSeparator={(sectionId, rowId) => {
+//           return <View key={rowId} style={styles.separator} />
+//         }}
+//       /> 
+//     ); 
+//   } 
+// } 
+
 export default class TodolistScreen extends Component {
   constructor(props) {
     super(props);
+
+    const ds = new ListView.DataSource({ 
+      rowHasChanged: (r1, r2) => r1 !== r2 
+    }); 
+
     this.state = {
       allComplete: false,
       value: "poooooo",
@@ -14,8 +68,34 @@ export default class TodolistScreen extends Component {
       items2: [],
       count: 0,
       pizza: '',
-      searchText: 'default input',
+      searchText: 'dyyyyyyyyyym',
       searchText2: '',
+      // dataSource: ds.cloneWithRows([])
+
+
+      dataSource: ds.cloneWithRows([ 
+        {
+          key: Date.now(),
+          text: "Tańczyć",
+          complete: false
+        }, 
+        {
+          key: Date.now(),
+          text: "Zapalać znicze",
+          complete: false
+        },
+        {
+          key: Date.now(),
+          text: "Wciągać dyyyyyyyyyym",
+          complete: false
+        }, 
+        // 'Walk the dog', 
+        // 'Do laundry', 
+        // 'Write the first chapter of my book' 
+      ]) 
+
+
+
       // name: 'name input',
       // stuffandthings: '',
       // username: '',
@@ -25,8 +105,10 @@ export default class TodolistScreen extends Component {
     // this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
     this.onPress = this.onPress.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    this.onSubmit2 = this.onSubmit2.bind(this);
     this.onPressDone = this.onPressDone.bind(this);
+    this.setSource = this.setSource.bind(this);
+
     // this.handleChange = this.handleChange.bind(this);
   }
 
@@ -34,6 +116,14 @@ export default class TodolistScreen extends Component {
   //   this.setState({
   //     count: this.state.count+1
   //   })
+
+  setSource(items2, itemsDataSource, otherState = { }) {
+    this.setState({
+      items2,
+      dataSource: this.state.dataSource.cloneWithRows(itemsDataSource),
+      ...otherState
+    })
+  }
 
   onPress() {
     this.setState({
@@ -44,42 +134,50 @@ export default class TodolistScreen extends Component {
   onSubmit(stuff) {
     // console.table({stuff});
     if(!{stuff}) return;
+    console.log('the stuff: ' + stuff);
     const newItems = [
       ...this.state.items,
       {
         key: Date.now(),
-        text: {stuff},
-        complete: false
+        text: stuff,
+        isComplete: false
       }
     ]
+    console.log(newItems)
+    console.log(this.state.items)
+
     this.setState({
       items: newItems,
       searchText: ''
       
     })
-    console.table({newItems});
-    console.table(newItems);
-    console.log(newItems);
+    // console.table({newItems});
+    // console.table(newItems);
+    // console.log(newItems);
   }
-  onSubmit2(stuff2) {
+
+  onSubmit2(stuff) {
     // console.table({stuff});
-    if(!{stuff2}) return;
+    if(!{stuff}) return;
     const newItems2 = [
       ...this.state.items2,
       {
         key: Date.now(),
-        text: {stuff2},
+        text: stuff,
         isComplete: false
       }
     ]
-    this.setState({
-      items2: newItems2,
-      searchText2: ''
+    console.log(newItems2)
+    console.log(this.state.items2)
+    this.setSource(newItems2, newItems2, {searchText2: ''})
+    // this.setState({
+    //   items2: newItems2,
+    //   searchText2: ''
       
-    })
-    console.table({newItems2});
-    console.table(newItems2);
-    console.log(newItems2);
+    // })
+    // console.table({newItems2});
+    // console.table(newItems2);
+    // console.log(newItems2);
   }
 
   onPressDone(stuff3) {
@@ -88,11 +186,11 @@ export default class TodolistScreen extends Component {
       ...item,
       isComplete
     }))
-    
-    this.setState({
-      items2: newItems2,
-      allComplete: isComplete
-    })
+    this.setSource(newItems2, newItems2, { allComplete: isComplete })
+    // this.setState({
+    //   items2: newItems2,
+    //   allComplete: isComplete
+    // })
 
     console.table({newItems2})
   }
@@ -166,6 +264,25 @@ export default class TodolistScreen extends Component {
               </TouchableOpacity>
             </View>
           </View>
+
+          <View>
+            <ListView 
+              style={styles.list}
+              enableEmptySections
+              onScroll={() => Keyboard.dismiss()}
+              renderSeparator={(sectionId, rowId) => {
+                return <View key={rowId} style={styles.separator} />
+              }}
+              dataSource={ this.state.dataSource } 
+              renderRow={ (item, ...values) => {
+                console.log(values);
+                console.log(item);
+                // return ( <Row text={text} {...values} /> )
+                return ( <Row item={item} {...values} /> );
+              }} 
+              
+            /> 
+          </View>
           
           <View style={styles.textinputbox}
             >
@@ -211,11 +328,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5F5F5'
-
   }, 
   content: {
     flex: 1,
     backgroundColor: '#89b9ba'
+  },
+  list: {
+    backgroundColor: '#FFF'
+  },
+  separator: {
+    borderWidth: 1,
+    borderColor: '#F5F5F5'
   },
   textinputbox: {
             flexDirection:'row', 
@@ -227,5 +350,14 @@ const styles = StyleSheet.create({
             borderColor:'#888', 
             borderRadius:10, 
             backgroundColor:'#fff'
+  },
+  listRow: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between'
+  },
+  textRow: {
+    fontSize: 24,
   }
 })
