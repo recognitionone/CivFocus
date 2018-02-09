fbe840b8980d56af382265f7ef9fbd4b407d332a
recognitionone <recognitionone@gmail.com>
2018-02-09 12:37:33 +0100

    new stuff

--- CivEx14/components/Stats/index.js
+++ CivEx14/components/Stats/index.js
@@ -10,16 +10,46 @@ export default class StatsScreen extends Component {
 		});
 
 		this.state = {
-			dataSource: ds.cloneWithRows(['row 1', 'row 2', 'row 3'])
+			dataSource: ds.cloneWithRows(['row 1', 'row 2', 'row 3']),
+			listOfTasks: [],
+			text: ''
 		};
 	}
+
+	_changeTextInputValue (text) { 
+    this.setState({ 
+      text 
+    }); 
+  } 
+
+	_addTask () { 
+    const listOfTasks = [...this.state.listOfTasks, this.state.text]; 
+
+    this.setState({ 
+      listOfTasks 
+    }); 
+
+    this._changeTextInputValue();
+  } 
 
 	render(){
 		const { navigate } = this.props.navigation;
 		return( 
 			<View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+				<View>
+					<TextInput 
+          autoCorrect={ false } 
+          onChangeText={ (text) => this._changeTextInputValue(text) } 
+          onSubmitEditing={ () => this._addTask() } 
+          returnKeyType={ 'done' } 
+          style={ styles.textInput } 
+          value={ this.state.text } 
+        	/>
+				</View>
+
 				<ListView 				
 					dataSource = { this.state.dataSource }
+					enableEmptySections={ true } 
 					renderRow = { (rowData) =>
 						<Text>{rowData}</Text>
 					}
